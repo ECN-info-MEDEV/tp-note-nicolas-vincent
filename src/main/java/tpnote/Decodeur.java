c4e5fe1f7a08bbfd5947f8e3dfd7797c1ffbe661
samysame <amal.samy.1997@gmail.com>
2023-01-26 10:49:07 +0100

    codeur points

--- src/main/java/tpnote/Decodeur.java
+++ src/main/java/tpnote/Decodeur.java
@@ -17,14 +17,13 @@ public class Decodeur extends Joueur {
         super(nom);
     }
 
-    /**
-     * main utilisé lors du dev pour test
-     * @param args 
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello");
-
-        Decodeur decodeur = new Decodeur("Nicolas");
-        decodeur.placer(new Ligne());
+    public int getPoints() {
+        return points;
     }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    
+
 }
