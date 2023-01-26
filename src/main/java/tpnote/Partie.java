2b484100819cdbd627d5ab82619994e1758ec107
samysame <amal.samy.1997@gmail.com>
2023-01-26 09:30:18 +0100

    début partie

--- src/main/java/tpnote/Partie.java
+++ src/main/java/tpnote/Partie.java
@@ -5,10 +5,37 @@
  */
 package tpnote;
 
+
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author samyamal
  */
 public class Partie {
+    
+    public static String[] couleurs = new  String[] {"jaune", "bleu", "rouge", "vert", "blanc", "noir"};
+    
+    private Decodeur decodeur;
+    private Codeur codeur;
+    private String[] code = new String[4];
+    private int toursRestants ;
+    
+    
+    /**
+     * Méthode permettant de démarrer une partie 
+     */
+    public void jeu(){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Quel est le nom du décodeur ?");
+        String nomDecodeur = scan.nextLine();
+        System.out.println("Quel est le nom du décodeur ?");
+       
+        
+        
+        
+    }
     
 }
