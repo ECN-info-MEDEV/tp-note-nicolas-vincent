3d3639961997caa45e032490fccd832ffc766739
vgros-jean <vince67000@gmail.com>
2023-01-26 09:19:35 +0100

    Documentation de Ligne

--- src/main/java/tpnote/Ligne.java
+++ src/main/java/tpnote/Ligne.java
@@ -6,21 +6,43 @@
 package tpnote;
 
 /**
- *
+ * Représente une ligne du jeu avec la proposition de 4 pions du décodeur et les fiches associés au décodage de cette proposition
  * @author samyamal
  */
 public class Ligne {
     
-        private String[] ligne = new String[4];
+    /**
+     * Code proposé par le décodeur, tableau de string avec les noms des couleurs
+     */
+    private String[] ligne = new String[4];
+    
+    /**
+     * nombre de fiche noires pour la ligne
+     */
     private int ficheN ;
+    
+    /**
+     * nombre de fiches blanches pour la ligne
+     */
     private int ficheB ;
 
+    /**
+     * constructeur complet pour une ligne
+     * @param ligne Code proposé par le décodeur
+     * @param ficheN nombre de fiche noires pour la ligne
+     * @param ficheB nombre de fiches blanches pour la ligne
+     */
     public Ligne(String[] ligne, int ficheN, int ficheB) {
         this.ligne = ligne;
         this.ficheN = ficheN;
         this.ficheB = ficheB;
     }
     
+    /**
+     * constructeur d'une ligne sans indiquer les flags (fiches)
+     * ceux-ci seront initialisés à zero
+     * @param ligne Code proposé par le décodeur
+     */
     public Ligne(String[] ligne) {
         this.ligne = ligne;
     }
