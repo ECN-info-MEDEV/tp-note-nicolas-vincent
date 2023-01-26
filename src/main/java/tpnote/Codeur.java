3301ca001552c3f06aac15e3f8d6f68147d123dc
samysame <amal.samy.1997@gmail.com>
2023-01-26 10:38:18 +0100

    codeur verifie

--- src/main/java/tpnote/Codeur.java
+++ src/main/java/tpnote/Codeur.java
@@ -5,22 +5,45 @@
  */
 package tpnote;
 
+import java.util.Scanner;
+
 /**
  *
  * @author samyamal
  */
 public class Codeur extends Joueur {
-    private int points;
+
+    private Scanner scan = new Scanner(System.in);
 
     public Codeur(String nom) {
         super(nom);
     }
 
-    public int getPoints() {
-        return points;
-    }
 
-    public void setPoints(int points) {
-        this.points = points;
+    
+    public Ligne verifie(Ligne ligne){
+        System.out.println("Le décodeur a placé les pions suivants :");
+        ligne.afficher();
+        System.out.println("Le codeur doit placer les fiches.");
+        ligne.setFicheN(this.nombreFiches("noires"));
+        ligne.setFicheB(this.nombreFiches("blanches"));
+        return ligne;
+    }
+    
+    public int nombreFiches(String couleur) {
+
+        System.out.println("Entrez le nombre de fiches "+couleur+" : ");
+        Integer nombreFiches;
+        try {
+            nombreFiches = Integer.parseInt(this.scan.nextLine());
+            if (nombreFiches < 5 && nombreFiches >= 0) {
+                System.out.println("Impossible");
+            }else{
+                return nombreFiches;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Je ne comprends pas.");
+        }
+        return nombreFiches(couleur);
     }
 }
